5ddaaf2197bf0ca82b47441db23d1bf3c5e1fb39
yashneet <yashneet.yashu@gmail.com>
2022-02-13 15:31:28 +0530

    new database connection details added to setup file

--- setup.py
+++ setup.py
@@ -5,15 +5,27 @@ mysql = MySQL()
 
 
 def setup_db(app):
+
     app.config['SECRET_KEY'] = 'TLS_AES_256_GCM_SHA384'
     app.config['MYSQL_HOST'] = 'sql6.freemysqlhosting.net'
-    app.config['MYSQL_USER'] = 'sql6470760'
-    app.config['MYSQL_PASSWORD'] = 'lWrGY6ZNE6'
-    app.config['MYSQL_DB'] = 'sql6470760'
+    app.config['MYSQL_USER'] = 'sql6472445'
+    app.config['MYSQL_PASSWORD'] = 'JSfi1hytFD'
+    app.config['MYSQL_DB'] = 'sql6472445'
     app.config['MYSQL_PORT'] = 3306
     app.config['JSON_SORT_KEYS'] = False
 
     mysql.init_app(app)
+
+# def setup_db(app):
+#     app.config['SECRET_KEY'] = 'TLS_AES_256_GCM_SHA384'
+#     app.config['MYSQL_HOST'] = 'sql6.freemysqlhosting.net'
+#     app.config['MYSQL_USER'] = 'sql6470760'
+#     app.config['MYSQL_PASSWORD'] = 'lWrGY6ZNE6'
+#     app.config['MYSQL_DB'] = 'sql6470760'
+#     app.config['MYSQL_PORT'] = 3306
+#     app.config['JSON_SORT_KEYS'] = False
+#
+#     mysql.init_app(app)
 
 # def setup_db(app):
 #     # app.config['SECRET_KEY'] = 'TLS_AES_256_GCM_SHA384'
