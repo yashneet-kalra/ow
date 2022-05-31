38dbb2ee460dfa18f3aa68306923b9401281c507
yashneet <yashneet.yashu@gmail.com>
2022-06-01 01:53:03 +0530

    created setup_psql_db method and updated requirements.txt

---
+++ setup_psql.py
@@ -0,0 +1,16 @@
+import psycopg2
+
+
+conn = None
+
+
+def setup_psql_db():
+    global conn
+
+    conn = psycopg2.connect(
+        host="ec2-52-204-195-41.compute-1.amazonaws.com",
+        database="d1vb6fgg31fa3v",
+        user="pftcrdgtxhxaqc",
+        password="d4e0e8ddb2349f87f06ebe977750573232e9bac6138d598240fe941f7462682a",
+        port="5432"
+    )
