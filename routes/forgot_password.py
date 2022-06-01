ea65bf3041acdf13f1561a7941824e392cf756ec
yashneet <yashneet.yashu@gmail.com>
2022-06-01 18:48:21 +0530

    changed database to psql in forgor_password.py

--- routes/forgot_password.py
+++ routes/forgot_password.py
@@ -1,5 +1,5 @@
 from flask import request, jsonify, Blueprint
-from setup import mysql
+from setup_psql import setup_psql_db
 
 
 forgot_password = Blueprint("forgot_password", __name__)
@@ -15,7 +15,8 @@ def forgot_pass():
     email = request.headers.get("email") or request.args.get("email")
 
     if email:
-        cur = mysql.connection.cursor()
+        conn = setup_psql_db()
+        cur = conn.cursor()
         cur.execute("SELECT EXISTS(SELECT * FROM users WHERE email=%s)", [email])
         data = cur.fetchone()
 
@@ -23,6 +24,7 @@ def forgot_pass():
             cur.execute("SELECT sec_question FROM users WHERE email=%s", [email])
             data = cur.fetchone()
             cur.close()
+            conn.close()
             return jsonify(
                 {
                     "sec_question": data[0],
@@ -31,6 +33,7 @@ def forgot_pass():
             ), 200
         else:
             cur.close()
+            conn.close()
             return jsonify(
                 {
                     "message": "User with this email does not exists",
@@ -52,12 +55,14 @@ def answer():
     email = request.headers.get("email") or request.args.get("email")
 
     if sec_answer and email:
-        cur = mysql.connection.cursor()
+        conn = setup_psql_db()
+        cur = conn.cursor()
         cur.execute("SELECT sec_answer FROM users WHERE email=%s", [email])
         data = cur.fetchone()
 
         if data[0] == sec_answer:
             cur.close()
+            conn.close()
             return jsonify(
                 {
                     "message": "Answer matched successfully",
@@ -66,6 +71,7 @@ def answer():
             ), 200
         else:
             cur.close()
+            conn.close()
             return jsonify(
                 {
                     "message": "Incorrect answer",
@@ -87,10 +93,12 @@ def change():
     new_pass = request.headers.get("new_pass") or request.args.get("new_pass")
 
     if email and new_pass:
-        cur = mysql.connection.cursor()
-        cur.execute("UPDATE users SET password=%s WHERE email=%s", ([new_pass], [email]))
-        mysql.connection.commit()
+        conn = setup_psql_db()
+        cur = conn.cursor()
+        cur.execute("UPDATE users SET password=%s WHERE email=%s", (new_pass, email))
+        conn.commit()
         cur.close()
+        conn.close()
 
         return jsonify(
             {
